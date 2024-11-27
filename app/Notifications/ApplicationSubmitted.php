ef6795f52f45612585bddbfbfd36ef24c7451d05
Yeeden <kezangyendon7002@gmail.com>
2024-11-27 12:56:35 +0600

    update

--- app/Notifications/ApplicationSubmitted.php
+++ app/Notifications/ApplicationSubmitted.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AccplicationSubmitted extends Notification
+class ApplicationSubmitted extends Notification
 {
     use Queueable;
 
