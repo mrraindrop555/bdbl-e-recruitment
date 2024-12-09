aabe6f1e7d5e9c13ee7a0acfe5167bd398904a18
Yeeden <kezangyendon7002@gmail.com>
2024-12-09 12:35:09 +0600

    update

--- app/Notifications/ApplicationRejected.php
+++ app/Notifications/ApplicationRejected.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ApplicationRejected extends Notification
+class ApplicationRejected extends Notification implements ShouldQueue
 {
     use Queueable;
 
